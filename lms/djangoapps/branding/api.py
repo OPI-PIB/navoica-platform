273ebd08ca7c35dd4290584df3cf193c81028105
Aleksander Majda <nazgut@gmail.com>
2018-10-26 11:34:39 +0200

    add contact page

--- lms/djangoapps/branding/api.py
+++ lms/djangoapps/branding/api.py
@@ -212,7 +212,7 @@ def _footer_navigation_links():
             #("blog", marketing_link("BLOG"), _("Blog")),
             #("news", marketing_link("NEWS"), _("News")),
             #("help-center", settings.SUPPORT_SITE_LINK, _("Help Center")),
-            ("contact", reverse("support:contact_us"), _("Contact")),
+            ("contact", marketing_link("CONTACT"), _("Contact")),
             #("careers", marketing_link("CAREERS"), _("Careers")),
             #("donate", marketing_link("DONATE"), _("Donate")),
         ]
