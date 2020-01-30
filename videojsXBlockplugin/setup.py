165e1c73205914b6aa0340414d5914a46e0c9fdf
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2020-01-30 18:58:48 +0100

    bump version videojs-xblock to 1.0

--- videojsXBlockplugin/setup.py
+++ videojsXBlockplugin/setup.py
@@ -16,7 +16,7 @@ def package_data(pkg, root):
 
 setup(
     name='videojs-xblock',
-    version='0.1.46',
+    version='1.0',
     description='XBlock to use the Video.js player in edX, instead of the default one.',
     packages=[
         'videojs',
