c16465e66b8a22934d906924ec0ac19c42089c8b
Krystian Łygas <krystian.lygas@opi.org.pl>
2020-03-13 16:20:26 +0100

    toogle show - hidden class for membership view

--- lms/static/js/instructor_dashboard/membership.js
+++ lms/static/js/instructor_dashboard/membership.js
@@ -1018,10 +1018,10 @@ such that the value can be defined later than this assignment (file load order).
                 ref1 = thismembership.auth_lists;
                 for (j = 0, len1 = ref1.length; j < len1; j++) {
                     authList = ref1[j];
-                    authList.$container.removeClass('active');
+                    authList.$container.addClass('d-none');
                 }
                 authList = $opt.data('auth_list');
-                authList.$container.addClass('active');
+                authList.$container.removeClass('d-none');
                 authList.re_view();
 
                 // On Change update the Group Moderation list
