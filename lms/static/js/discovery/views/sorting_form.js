b013d46c1d99d91663c469177b365aa06c0b641a
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2020-04-13 18:29:58 +0200

    sorting courses

---
+++ lms/static/js/discovery/views/sorting_form.js
@@ -0,0 +1,34 @@
+(function(define) {
+    define(['jquery', 'backbone', 'gettext'], function($, Backbone, gettext) {
+        'use strict';
+
+        return Backbone.View.extend({
+
+            el: '#sorting-form',
+            events: {
+                'change select': 'sortListOption',
+            },
+
+            initialize: function() {
+                this.$searchField = this.$el.find('input');
+                this.$searchButton = this.$el.find('button');
+                this.$message = this.$el.find('#discovery-message');
+                this.$loadingIndicator = this.$el.find('#loading-indicator');
+            },
+
+            sortListOption: function(event) {
+                var $target = $(event.currentTarget);
+                $target = $target.find('option:selected');
+                this.trigger(
+                'sortListOption',
+                $target.val(),
+            );
+            },
+
+            doSort: function() {
+                this.trigger('search', $.trim(term));
+            },
+
+        });
+    });
+}(define || RequireJS.define));
