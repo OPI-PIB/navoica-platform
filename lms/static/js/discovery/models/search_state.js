8de1e3cef78467d30612555111ad454dd2431d26
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2020-04-20 15:40:23 +0200

    fix wrong comparision for course_discovery.js

--- lms/static/js/discovery/models/search_state.js
+++ lms/static/js/discovery/models/search_state.js
@@ -11,7 +11,7 @@
         return Backbone.Model.extend({
 
             page: 0,
-            pageSize: 20,
+            pageSize: 100,
             searchTerm: '',
             terms: {},
             jqhxr: null,
