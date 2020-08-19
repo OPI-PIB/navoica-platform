be61b7627fb20f383961c8fd63e65d9601e24d70
Krystian Łygas <krystian.lygas@opi.org.pl>
2020-08-19 11:55:14 +0200

    [ANG#1310] added new po sources for filter for course_catalog [ANG#1310] moved sorting function of course_organizer and course_category to course.py [ANG#1310] gettexted course_catalog filters attributes and removed sorting functions [IMP#1310] extended EdxJSONEncoder by possibility to serialize gettext variable

--- common/lib/xmodule/xmodule/modulestore/__init__.py
+++ common/lib/xmodule/xmodule/modulestore/__init__.py
@@ -1452,3 +1452,36 @@ class EdxJSONEncoder(json.JSONEncoder):
                 return obj.isoformat()
         else:
             return super(EdxJSONEncoder, self).default(obj)
+
+
+# using load_function from django module, for importing force_text and Promise django functions
+from .django import load_function
+force_text = load_function('django.utils.encoding.force_text')
+Promise = load_function('django.utils.functional.Promise')
+
+
+class EdxJSONEncoder(json.JSONEncoder):
+    """
+    Custom JSONEncoder that handles `Location` and `datetime.datetime` objects.
+
+    `Location`s are encoded as their url string form, and `datetime`s as
+    ISO date strings
+    """
+
+    def default(self, obj):
+        if isinstance(obj, (CourseKey, UsageKey)):
+            return unicode(obj)
+        elif isinstance(obj, datetime.datetime):
+            if obj.tzinfo is not None:
+                if obj.utcoffset() is None:
+                    return obj.isoformat() + 'Z'
+                else:
+                    return obj.isoformat()
+            else:
+                return obj.isoformat()
+        # extending EdxJSONEncoder by option of serializing gettext variables
+        elif isinstance(obj, Promise):
+            return force_text(obj)
+        else:
+            return super(EdxJSONEncoder, self).default(obj)
+
