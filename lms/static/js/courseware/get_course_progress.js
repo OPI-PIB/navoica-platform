982621e93b5c30dc2d61b54595c8fc6b5669c911
paula-opi <Paula.Wasylow@opi.org.pl>
2020-09-09 13:00:30 +0200

    fixed update progress in other tabs on change user

--- lms/static/js/courseware/get_course_progress.js
+++ lms/static/js/courseware/get_course_progress.js
@@ -11,7 +11,26 @@ function getCourseProgressReq(usernameArg, courseIdArg, urlOriginArg) {
         let patternValidation = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
         if (learnerUsernameValue !== '' && !patternValidation.test(learnerUsernameValue)) {
             username = $learnerUsername.val();
+            sendRequest(username, courseId);
         }
+    }
+
+    const sendRequest = function(username, courseId) {
+        let endpoint = `/api/navoica/v1/progress/${username}/courses/${courseId}`;
+
+        $.ajax({
+            url: urlOriginPath + endpoint,
+            type: 'GET',
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function(result){
+                if (result) {
+                    let percentage = Math.round(result.completion_value * 100);
+                    $('.progress-bar').css('width', percentage + '%').attr('aria-valuenow', percentage);
+                    $('.js-percentage-progress').text(percentage + '%');
+                }
+            }
+        });
     }
 
     $learnerUsername.on('change', function() {
@@ -26,21 +45,7 @@ function getCourseProgressReq(usernameArg, courseIdArg, urlOriginArg) {
         }, 350);
     }
 
-    let endpoint = `/api/navoica/v1/progress/${username}/courses/${courseId}`;
-
-    $.ajax({
-        url: urlOriginPath + endpoint,
-        type: 'GET',
-        contentType: 'application/json',
-        dataType: 'json',
-        success: function(result){
-            if (result) {
-                let percentage = Math.round(result.completion_value * 100);
-                $('.progress-bar').css('width', percentage + '%').attr('aria-valuenow', percentage);
-                $('.js-percentage-progress').text(percentage + '%');
-            }
-        }
-    });
+    sendRequest(username, courseId);
 }
 
 
