818346209cf445e8b6246f2991a8b4eddad03bf7
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2020-10-13 14:00:16 +0200

    update k8s; change tracking handler and use env for PLATFORM_VERSION

--- lms/envs/k8s.py
+++ lms/envs/k8s.py
@@ -1,4 +1,6 @@
 from .aws import *
+from logging.handlers import SysLogHandler
+import os
 
 LOGGING['handlers']['local'] = {
     'level': 'INFO',
@@ -9,4 +11,8 @@ LOGGING['handlers']['tracking'] = {
     'level': 'DEBUG',
     'class': 'logging.handlers.SysLogHandler',
     'address': ('192.168.51.52', 514),
+    'facility': SysLogHandler.LOG_LOCAL1,
+    'formatter': 'raw',
 }
+
+PLATFORM_VERSION = os.environ['BUILD_TAG']
