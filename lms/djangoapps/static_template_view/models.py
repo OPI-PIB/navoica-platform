ed465dcad6d231f2c6b237fc9dd64562327aa0f2
Aleksander Majda <nazgut@gmail.com>
2021-01-10 21:22:47 +0100

    change database model and addsimple success styling

--- lms/djangoapps/static_template_view/models.py
+++ lms/djangoapps/static_template_view/models.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.utils.translation import gettext_lazy as _
-from django.core.exceptions import ValidationError
-from django.core.validators import validate_email
 from django.forms import ModelForm
 
 class Newsletter_emails(models.Model):
@@ -10,15 +7,7 @@ class Newsletter_emails(models.Model):
         app_label = "static_template_view"
 
     created_at = models.DateTimeField(auto_now_add=True)
-    email = models.CharField(max_length=255)
-
-    def clean(self):
-        try:
-            validate_email( self.email )
-            return True
-        except ValidationError:
-            raise ValidationError(_('You must enter valid E-mail address.'))
-            return False
+    email = models.EmailField(max_length=254)
 
 class Newsletter_emailsForm(ModelForm):
     class Meta:
