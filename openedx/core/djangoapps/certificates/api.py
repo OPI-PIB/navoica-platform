5a15a0c28ec3a2cad9ba3e9f789036a101973baf
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2021-02-11 14:59:06 +0100

    FIX: use modified_date for certificate preview

--- openedx/core/djangoapps/certificates/api.py
+++ openedx/core/djangoapps/certificates/api.py
@@ -104,4 +104,6 @@ def display_date_for_certificate(course, certificate):
     ):
         return course.certificate_available_date
 
-    return certificate.created_date
+    if certificate.created_date:
+        return certificate.created_date
+    return certificate.modified_date
