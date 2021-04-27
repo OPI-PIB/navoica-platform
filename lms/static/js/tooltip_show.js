d6d0706514d00375c6d94d90423620809bf89128
kaczmarczyk-malgorzata <53815753+kaczmarczyk-malgorzata@users.noreply.github.com>
2021-04-27 13:23:28 +0200

    MOOC-1927 Style: add tooltip to progress bar (#542)

---
+++ lms/static/js/tooltip_show.js
@@ -0,0 +1,52 @@
+function showTip() {
+  var tip = document.getElementById("tooltip");
+  var btntip = document.getElementById("btntip");
+  var message = btntip.getAttribute('tooltip-message');
+  btntip.setAttribute("aria-expanded", "true");
+  tip.innerHTML=message; 
+  tip.setAttribute("style", "display: block;");
+}
+
+function hideTip() {
+  var tip = document.getElementById("tooltip");
+  var btntip = document.getElementById("btntip");
+  btntip.setAttribute("aria-expanded", "false");
+  tip.innerHTML = "";
+  tip.setAttribute("style", "display: none;");
+}
+
+function toggleTip (event) {
+  var tip = document.getElementById("tooltip");
+  if (window.getComputedStyle(tip).getPropertyValue('display') === 'none') {
+    showTip();
+  }
+  else {
+    hideTip();	
+  }
+}
+
+$(document).ready(function() {
+
+  var btntip = document.getElementById("btntip");
+  
+  btntip.addEventListener("mousemove", function (event) {
+    showTip();
+  });
+
+  btntip.addEventListener("mouseout", function (event) {
+    hideTip();
+  });
+
+  btntip.addEventListener("blur", function (event) {
+    hideTip();
+  });
+
+  btntip.addEventListener("click", toggleTip, false);
+
+  btntip.addEventListener('keyup', function (event){
+    let key = event.which || event.keyCode || event.key;
+    if (key === 27) {
+      hideTip();
+    }
+  });
+});
