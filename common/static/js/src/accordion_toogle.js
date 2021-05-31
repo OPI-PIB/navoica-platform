2789ae00a088cfabed0584839af0babbe2c589ef
krystianlygas <51002868+krystianlygas@users.noreply.github.com>
2021-05-31 16:04:08 +0200

    MOOC-2132 Fix: add accordion script to base_vendor scripts (#561)

---
+++ common/static/js/src/accordion_toogle.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.parentElement.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+});
